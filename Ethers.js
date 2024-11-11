59b66fc45c4e78de32e2735526d17f2b2b3d57be
AnticriptoBuildbear <orlanduisayala@gmail.com>
2024-11-10 22:28:23 -0500

    Add files via upload

---
+++ Ethers.js
@@ -0,0 +1,3 @@
+const provider = new ethers.JsonRpcProvider("https://rpc.buildbear.io/curly-sandman-10c6e11a");
+const blockNumber = await provider.getBlockNumber();21131730
+console.log(21131730)
---
+++ Wagmi.js
@@ -0,0 +1,34 @@
+  const mainnet = 
+        id: 21548,
+        name: "curly-sandman-10c6e11a",
+        network: "sandman mainnet",
+        nativeCurrency: {
+          decimals: 18,
+          name: "Ethereum",
+          symbol: "ETH",
+        },
+        rpcUrls: {
+          public: { http: ["https://rpc.buildbear.io/curly-sandman-10c6e11a"] },
+          default: { http: ["https://rpc.buildbear.io/curly-sandman-10c6e11a"] },
+        },
+        blockExplorers: {
+          etherscan: {
+            name: "etherscan",
+            url: "etherscan.io",
+          },
+          default: {
+            name: "Curly sand man"       
+          } ,url:"https:curly-sandman-10c6e11a.blockscout.buildbear.io",
+        },
+      } as const satisfies Chain; 
+      
+  const { chains, publicClient, webSocketPublicClient } = configureChains( "ethereum"
+        [mainnet],
+        [
+          jsonRpcProvider({
+            rpc: (chain) => ({
+              http: "https://rpc.buildbear.io/curly-sandman-10c6e11a",
+            }),
+          }),
+        ]
+      );
